84ddcca1dc0e448f214d03e2ea24975287114a94
eliaswound <eliaswound@gmail.com>
2024-10-31 20:10:03 -0500

    1

--- main.py
+++ main.py
@@ -4,9 +4,6 @@
 # Part 1: File input if needed
 # Import anything you need for getting your file input/output)
 
-from PyQt5.QtWidgets import QApplication
-from run_Functions.run_Initial_Settings import run_init_settings  # Import the function
-import sys
 
 if __name__ == "__main__":
     run_init_settings()
